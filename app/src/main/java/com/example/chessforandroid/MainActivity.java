51a582cf2dbeda62ff56284920f13f91c330fd95
chemecador <alejandrogata7@gmail.com>
2022-03-14 00:50:54 +0100

    CHESSForAndroid iniciado

--- app/src/main/java/com/example/chessforandroid/MainActivity.java
+++ app/src/main/java/com/example/chessforandroid/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.chessforandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button twoPlayers;
+    Button online;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        twoPlayers = findViewById(R.id.bMain2P);
+        online = findViewById(R.id.bMainOnline);
+
+        twoPlayers.setOnClickListener(this);
+        online.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bMain2P:
+                Toast.makeText(getApplicationContext(),
+                        "Función no disponible :(",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bMainOnline:
+                Toast.makeText(getApplicationContext(),
+                        "Función tampoco disponible :(",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
