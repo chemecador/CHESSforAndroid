8325b6e3951b68bd88fc96861b467534e3b20b41
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-05-11 18:25:45 +0200

    Update Cifrado.java

--- server/src/main/java/servidor/Cifrado.java
+++ server/src/main/java/servidor/Cifrado.java
@@ -7,11 +7,12 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-/***
+/**
  * Librería Cifrado. Contiene 2 métodos fundamentales: Cifrar y Descifrar.
  */
 public class Cifrado {
-    //atributos de la clase Cifrado, nos sirven para cifrar y descifrar con la misma clave
+    // atributos de la clase Cifrado, nos sirven para cifrar y descifrar con la
+    // misma clave
     private KeyGenerator keyGenerator;
     private Key key;
     private Cipher aes;
@@ -28,9 +29,6 @@ public class Cifrado {
             // y nos quedamos con los bytes 0 a 15
             key = new SecretKeySpec("una clave de 16 bytes".getBytes(), 0, 16, "AES");
 
-            // Ver como se puede guardar esta clave en un fichero y recuperarla
-            // posteriormente en la clase RSAAsymetricCrypto.java
-
             // Se obtiene un cifrador AES
             aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
         } catch (NoSuchAlgorithmException e) {
@@ -41,9 +39,12 @@ public class Cifrado {
     }
 
     /***
-     * Recibe un mensaje por parámetro y lo cifra con el algoritmo AES, codificándolo posteriormente en Base64
+     * Recibe un mensaje por parámetro y lo cifra con el algoritmo AES,
+     * codificándolo posteriormente en Base64
      * para su optimización y hacer legibles sus caracteres.
-     * Esto permitirá añadirlo a una base de datos o trabajar con él más cómodamente.
+     * Esto permitirá añadirlo a una base de datos o trabajar con él más
+     * cómodamente.
+     * 
      * @param s Mensaje de tipo String que queremos cifrar
      * @return String con el mensaje cifrado
      */
@@ -53,27 +54,28 @@ public class Cifrado {
             // Se inicializa para encriptacion y se encripta el texto,
             // que debemos pasar como bytes.
             aes.init(Cipher.ENCRYPT_MODE, key);
-            // Guardamos en un array de bytes lo que nos devuelve el método doFinal, que también es un array de bytes.
-            // Como parámetro le enviamos el mensaje que queremos cifrar, parseado a un array de bytes.
+            // Guardamos en un array de bytes lo que nos devuelve el método doFinal, que
+            // también es un array de bytes.
+            // Como parámetro le enviamos el mensaje que queremos cifrar, parseado a un
+            // array de bytes.
             byte[] encriptado = aes.doFinal(s.getBytes());
 
             // Devolvemos el mensaje codificado en Base64 y lo convertido a string
             return Base64.getEncoder().encodeToString(encriptado);
-        } catch (
-                InvalidKeyException e) {
+        } catch (InvalidKeyException e) {
             e.printStackTrace();
-        } catch (
-                IllegalBlockSizeException e) {
+        } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
-        } catch (
-                BadPaddingException e) {
+        } catch (BadPaddingException e) {
             e.printStackTrace();
         }
         return null;
     }
+
     /***
      * Recibe un mensaje cifrado por parámetro y lo descifra con el algoritmo AES.
      * Pre: El mensaje recibido debe estar codificado en Base64.
+     * 
      * @param s Mensaje de tipo String que queremos descifrar
      * @return String con el mensaje descifrado
      */
@@ -85,7 +87,8 @@ public class Cifrado {
             aes.init(Cipher.DECRYPT_MODE, key);
 
             // Como el mensaje recibido está en Base64, el primer paso es descodificarlo
-            // Una vez hecho, se lo enviamos como parámetro al método doFinal que nos devuelve
+            // Una vez hecho, se lo enviamos como parámetro al método doFinal que nos
+            // devuelve
             // el mensaje descifrado en un array de bytes
             byte[] desencriptado = aes.doFinal(Base64.getDecoder().decode(s.getBytes()));
             // Devolvemos el mensaje descifrado en un string
